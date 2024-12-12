8d3bf60535bd6d9b0f066a8983909f5169ee0836
Marek Macznik <marek.macznik@gmail.com>
2024-12-13 00:47:14 +0100

    Add: setup.lua

---
+++ lua/setup.lua
@@ -0,0 +1,16 @@
+local function install_dependencies()
+  local dependencies = {"lua-http", "lua-ssl", "dkjson"}
+  for _, dep in ipairs(dependencies) do
+    local command = "luarocks install " .. dep
+    local result = vim.fn.system(command)
+
+    if vim.v.shell_error ~= 0 then
+      print("Error installing " .. dep .. ": " .. result)
+    else
+      print(dep .. " installed successfully!")
+    end
+  end
+end
+
+install_dependencies()
+
