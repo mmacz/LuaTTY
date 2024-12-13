17ce5eaa8b2ea1d957cdbfc040705c3e1195960b
Marek Macznik <marek.macznik@gmail.com>
2024-12-13 11:44:31 +0100

    Rework api

--- lua/luatty/setup.lua
+++ lua/luatty/setup.lua
@@ -4,12 +4,14 @@ local function install_dependencies()
         local command = "luarocks install " .. dep
         local result = vim.fn.system(command)
         if vim.v.shell_error ~= 0 then
-            vim.notify("Error installing " .. dep .. ": " .. result, vim.log.levels.ERROR)
+            vim.notify("Failed to install " .. dep .. ": " .. result, vim.log.levels.ERROR)
         else
-            vim.notify(dep .. " installed successfully!", vim.log.levels.INFO)
+            vim.notify(dep .. " installed successfully", vim.log.levels.INFO)
         end
     end
 end
 
-install_dependencies()
+return {
+    install_dependencies = install_dependencies,
+}
 
